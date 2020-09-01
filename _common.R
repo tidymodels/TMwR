1a4cb89ede8546b415a5cc4c21361e6885161bb3
Max Kuhn <mxkuhn@gmail.com>
2020-09-01 13:35:22 -0400

    potential better tibble printing

--- _common.R
+++ _common.R
@@ -1,6 +1,7 @@
 options(digits = 4, width = 84)
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 options(cli.width = 85)
+options(crayon.enabled = FALSE)
 
 knitr::opts_chunk$set(
   comment = "#>",
