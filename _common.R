be9088e791fc39441197c0ac97b83d079f87d16a
Julia Silge <julia.silge@gmail.com>
2022-03-03 12:36:32 -0700

    Go back to PNG

--- _common.R
+++ _common.R
@@ -7,11 +7,7 @@ knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   fig.align = 'center',
-  tidy = FALSE,
-  dev = "svglite",
-  dev.args = list(web_fonts = list(
-    'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
-  ))
+  tidy = FALSE
 )
 
 
